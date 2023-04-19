2823d8b9889dc94528bc5650750638539d374b5e
Fernando Meyer <fm@pobox.com>
2023-04-19 02:09:07 +0200

    feat: working prototype

--- src/main.rs
+++ src/main.rs
@@ -1,89 +1,22 @@
+use std::env;
+use std::fs;
+use std::io::{self, Read, stdout, Write};
+use std::process::Command;
+
+use chatgpt::prelude::*;
+use futures_util::stream::StreamExt;
+use regex::{Error, Regex};
 use reqwest::Client;
 use serde::{Deserialize, Serialize};
 use serde_json::json;
-use std::env;
-use std::fs;
-use std::io::{self, Read};
-use std::process::Command;
 use tokio;
-use regex::Regex;
+use tokio::macros::support;
+use std::process::Stdio;
+use std::fs::File;
 
-
-#[derive(Debug, Deserialize, Serialize)]
-struct OpenAIRequest {
-    messages: Vec<Message>,
-}
-
-#[derive(Debug, Deserialize, Serialize)]
-struct Message {
-    role: String,
-    content: String,
-}
-
-#[derive(Debug, Deserialize)]
-struct OpenAIResponse {
-    choices: Vec<Choice>,
-}
-
-#[derive(Debug, Deserialize)]
-struct Choice {
-    message: Message,
-}
-
-async fn get_summary(api_key: &str, text: &str) -> Result<String, Box<dyn std::error::Error>> {
-    let api_url = "https://api.openai.com/v1/chat/completions";
-
-    let client = Client::new();
-
-    let prompt = format!(
-        "sumarize the following content, in a structured outline way\n\n{}",
-        text
-    );
-
-    let req_body = OpenAIRequest {
-        messages: vec![
-            Message {
-                role: "system".to_string(),
-                content: "You are ChatGPT, a large language model trained by OpenAI.".to_string(),
-            },
-            Message {
-                role: "user".to_string(),
-                content: prompt.to_string(),
-            },
-        ],
-    };
-
-    let response = client
-        .post(api_url)
-        .header("Content-Type", "application/json")
-        .header("Authorization", format!("Bearer {}", api_key))
-        .json(&json!({
-            "messages": req_body.messages,
-            "model": "gpt-3.5-turbo",
-            "max_tokens": 2046,
-            "temperature": 0.7,
-            "stop": ["\n"]
-        }))
-        .send()
-        .await?;
-
-    // println!("{}", response.text().await?);
-
-    let openai_response: OpenAIResponse = response.json().await?;
-
-    for m in openai_response.choices {
-        println!("Summary and key takeaways:\n{}", m.message.content);
-    }
-
-    let buffer = String::new();
-
-
-    Ok(buffer.clone())
-}
-
-fn cleanup_buffer(text: &str) -> String {
+fn cleanup_buffer(text: String) -> String {
     let timestamp_regex = Regex::new(r"(\d{2}:\d{2}:\d{2}\.\d{3} --> \d{2}:\d{2}:\d{2}\.\d{3}\n)").unwrap();
-    let cleaned_text = timestamp_regex.replace_all(text, "");
+    let cleaned_text = timestamp_regex.replace_all(text.as_str(), "");
     let nbsp_regex = Regex::new(r"&nbsp;").unwrap();
     let cleaned_text = nbsp_regex.replace_all(&cleaned_text, " ");
     let empty_line_regex = Regex::new(r"(?m)^\s*\n").unwrap();
@@ -91,16 +24,19 @@ fn cleanup_buffer(text: &str) -> String {
     cleaned_text.into_owned()
 }
 
-
-fn main() {
+#[tokio::main]
+async fn main() -> Result<()> {
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
         eprintln!("Usage: {} <video_url>", args[0]);
-        return;
+        return Ok(());
     }
 
     let video_url = &args[1];
     let output_file = "output.en.vtt";
+
+    let file = File::create("out.txt").unwrap();;
+    let stdio = Stdio::from(file);
 
     let status = Command::new("yt-dlp")
         .args(&[
@@ -113,32 +49,97 @@ fn main() {
             "output",
             video_url,
         ])
+        .stdout(stdio)
         .status()
         .expect("Failed to execute yt-dlp command");
 
+
+
+
+
+
     if status.success() {
-        let mut buffer = String::new();
-        // Remove annotated timestamps
-        let timestamp_regex = Regex::new(r"(\d{2}:\d{2}:\d{2}\.\d{3} --> \d{2}:\d{2}:\d{2}\.\d{3}\n)").unwrap();
 
+        // cat command
+        let cat = Command::new("cat")
+            .arg(output_file)
+            .stdout(Stdio::piped())
+            .spawn()
+            .expect("Failed to execute cat command");
 
-        let mut file = fs::File::open(&output_file).expect("Failed to open subtitle file");
-        file.read_to_string(&mut buffer)
-            .expect("Failed to read subtitle file");
+        // grep command
+        let grep = Command::new("grep")
+            .arg("-v")
+            .arg(":")
+            .stdin(cat.stdout.unwrap())
+            .stdout(Stdio::piped())
+            .spawn()
+            .expect("Failed to execute grep command");
 
+        // awk command
+        let awk = Command::new("awk")
+            .arg("!seen[$0]++")
+            .stdin(grep.stdout.unwrap())
+            .stdout(Stdio::piped())
+            .spawn()
+            .expect("Failed to execute awk command");
 
-        let cleaned_buffer = cleanup_buffer(&buffer);
+        let output = awk.wait_with_output().expect("Failed to wait on awk command");
+
+        let cleaned_text = cleanup_buffer(String::from_utf8_lossy(&output.stdout).to_string());
+        // println!("Cleaned text:\n{}", cleaned_text);
 
         // Clean up regular file
         fs::remove_file(output_file).expect("Failed to remove subtitle file");
 
         let api_key =
             env::var("OPENAI_API_KEY").expect("Missing OPENAI_API_KEY environment variable");
-        let summary = tokio::runtime::Runtime::new()
-            .unwrap()
-            .block_on(get_summary(&api_key, &cleaned_buffer))
-            .expect("Failed to get summary from ChatGPT");
+
+        // let support = get_summary(&api_key, &cleaned_buffer).await;
+
+        let client = ChatGPT::new_with_config(
+            api_key,
+            ModelConfigurationBuilder::default()
+                .temperature(0.7)
+                .engine(ChatGPTEngine::Gpt35Turbo_0301)
+                .build()
+                .unwrap(),
+        )?;
+
+
+// Acquiring a streamed response
+// Note, that the `futures_util` crate is required for most
+// stream related utility methods
+
+        println!("processing ....");
+        let prompt = format!(
+            "sumarize the following content, in a structured outline way\n\n{}",
+            cleaned_text
+        );
+        let stream = client
+            .send_message_streaming(prompt)
+            .await?;
+
+// Iterating over stream contents
+        stream
+            .for_each(|each| async move {
+                match each {
+                    ResponseChunk::Content {
+                        delta,
+                        response_index: _,
+                    } => {
+                        // Printing part of response without the newline
+                        print!("{delta}");
+                        // Manually flushing the standard output, as `print` macro does not do that
+                        stdout().lock().flush().unwrap();
+                    }
+                    _ => {}
+                }
+            })
+            .await;
     } else {
         eprintln!("yt-dlp command failed with status: {}", status)
     }
+
+    Ok(())
 }
